ad2b038e2982f0f46408c7e495e3448ee573bb7a
Julianaros <89995618+Julianaros@users.noreply.github.com>
2024-09-11 09:53:09 -0500

    Cambiando las respectivas duplas para que coincidan con el archivo Final_Final_Final_ESTE_SI resultados.py

--- resultados.py
+++ resultados.py
@@ -9,8 +9,7 @@ t = np.linspace(0,20,400)
 x1_analitica, x2_analitica = analitica(t)
 
 # Dupla soluciones numéricas
-euler, rk4 = numerica(t)
-
+euler_result, rk4_result = numerica(t)
 # Crear una figura y un arreglo de subgráficas (1 fila con 3 columnas)
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 12))
 
@@ -22,15 +21,15 @@ ax1.set_ylabel("Posición (m)")
 ax1.set_title("Oscilador Acoplado - Solución Analítica")
 ax1.legend()
 
-ax2.plot(t, euler[0], label='x1 (Euler)', linestyle='-', color='green')
-ax2.plot(t, euler[1], label='x2 (Euler)', linestyle='-', color='orange')
+ax2.plot(t, euler_result[0], label='x1 (Euler)', linestyle='-', color='green')
+ax2.plot(t, euler_result[1], label='x2 (Euler)', linestyle='-', color='orange')
 ax2.set_xlabel("Tiempo (s)")
 ax2.set_ylabel("Posición (m)")
 ax2.set_title("Oscilador Acoplado - Método de Euler")
 ax2.legend()
 
-ax3.plot(t, rk4[0], label='x1 (RK4)', linestyle='-', color='purple')
-ax3.plot(t, rk4[1], label='x2 (RK4)', linestyle='-', color='green')
+ax3.plot(t, rk4_result[0], label='x1 (RK4)', linestyle='-', color='purple')
+ax3.plot(t, rk4_result[1], label='x2 (RK4)', linestyle='-', color='green')
 ax3.set_xlabel("Tiempo (s)")
 ax3.set_ylabel("Posición (m)")
 ax3.set_title("Oscilador Acoplado - Método Runge-Kutta 4")
